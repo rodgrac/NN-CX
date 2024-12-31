efbb5d8287ed0f27aebddd4a918cbfc2d7fa773d
rodgrac <rodneydsouza92@gmail.com>
2024-12-30 21:17:23 -0800

    Add linear and activation ops impl.

---
+++ nncl/layers/activations.py
@@ -0,0 +1,37 @@
+from typing import Any
+import numpy as np
+
+from nncl.tensor import Tensor
+
+
+class ReLU:
+    def __call__(self, x) -> Any:
+        return self.forward(x)
+    
+    def forward(self, x):
+        out = Tensor(data=np.maximum(0, x.data), grad_en=x.grad_en)
+        
+        if out.grad_en:
+            out.grad_fn = lambda grad: [grad * (x.data > 0)]
+            out._prev = [x]
+        
+        return out
+    
+    def __repr__(self) -> str:
+        return f"ReLU"
+        
+class Sigmoid:
+    def __call__(self, x) -> Any:
+        return self.forward(x)
+    
+    def forward(self, x):
+        out = Tensor(1/(1 + np.exp(-x.data)), grad_en=x.grad_en)
+        
+        if out.grad_en:
+            out.grad_fn = lambda grad: [grad * out.data * (1 - out.data)]
+            out._prev = [x]
+        
+        return out
+    
+    def __repr__(self) -> str:
+        return f"Sigmoid"
---
+++ nncl/layers/linear.py
@@ -0,0 +1,27 @@
+from typing import Any
+from nncl.tensor import Tensor
+
+
+class Linear:
+    def __init__(self, in_features, out_features, bias=True) -> None:
+        self.w = Tensor(shape=(in_features, out_features), grad_en=True).randn()
+        if bias:
+            self.bias = Tensor(shape=(1, out_features), grad_en=True)
+        else:
+            self.bias = None
+            
+        self._params = [self.w, self.bias] if self.bias is not None else [self.w]
+        
+    def __call__(self, x) -> Any:
+        return self.forward(x)
+    
+    def forward(self, x):
+        x = x @ self.w
+        if self.bias is not None:
+            x = x + self.bias
+        
+        return x
+    
+    def __repr__(self) -> str:
+        return f"Linear({self.w.shape}, bias={self.bias!=None})"
+    
