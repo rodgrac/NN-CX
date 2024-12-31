cd1ee0a6f7ee0788a7d72d43fd5d1243f522347b
rodgrac <rodneydsouza92@gmail.com>
2024-12-30 21:19:02 -0800

    Fix bug in utils topo_sort

--- nncl/utils.py
+++ nncl/utils.py
@@ -6,12 +6,15 @@ def topo_sort(node):
         if node not in visited:
             visited.add(node)
             for child in node._prev:
-                dfs(child)
+                if child is not None:
+                    dfs(child)
             sorted_nodes.append(node)
-            
+                    
     dfs(node)
-                
+
     return sorted_nodes
+
+
         
     
     
