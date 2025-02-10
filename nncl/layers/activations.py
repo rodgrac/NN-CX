386aed8a0294b9dc022d68fe48de67e4529aa841
rodgrac <rodneydsouza92@gmail.com>
2025-02-09 22:28:29 -0800

    Add tanh op support

--- nncl/layers/activations.py
+++ nncl/layers/activations.py
@@ -35,3 +35,19 @@ class Sigmoid:
     
     def __repr__(self) -> str:
         return f"Sigmoid"
+    
+class Tanh:
+    def __call__(self, x) -> Any:
+        return self.forward(x)
+    
+    def forward(self, x):
+        out = Tensor((np.exp(x.data) - np.exp(-x.data)) /(np.exp(x.data) + np.exp(-x.data)), grad_en=x.grad_en)
+        
+        if out.grad_en:
+            out.grad_fn = lambda grad: [grad * (1 - out.data**2.0)]
+            out._prev = [x]
+        
+        return out
+    
+    def __repr__(self) -> str:
+        return f"Tanh"
