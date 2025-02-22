3556f18e78c2fa08d25931ad9e538ce047825859
rodgrac <rodneydsouza92@gmail.com>
2025-02-21 23:29:29 -0800

    Add support for data transforms

---
+++ nncx/datasets/transform.py
@@ -0,0 +1,21 @@
+class Transform:
+    def __call__(self, tensor):
+        raise NotImplementedError
+    
+class Standardize(Transform):
+    def __init__(self, mean, std):
+        super().__init__()
+        self.mean = mean
+        self.std = std
+        
+    def __call__(self, tensor):
+        return (tensor - self.mean) / self.std
+
+class Normalize(Transform):
+    def __init__(self, min_val, max_val):
+        super().__init__()
+        self.min_val = min_val
+        self.max_val = max_val
+        
+    def __call__(self, tensor):
+        return (tensor - self.min_val) / (self.max_val - self.min_val)
