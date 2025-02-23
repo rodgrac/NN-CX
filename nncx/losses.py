c94f781ba72d584da9b2ef3bc08ac86b3bf81acb
rodgrac <rodneydsouza92@gmail.com>
2025-02-23 01:27:38 -0800

    Add cross entropy loss

--- nncx/losses.py
+++ nncx/losses.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 from nncx.tensor import Tensor
+from nncx.layers.activations import SoftMax
 
 
 class MSELoss:
@@ -14,4 +15,25 @@ class MSELoss:
             loss.grad_fn = lambda grad: [grad * 2 * (pred.data - target.data) / target.size]
             loss._prev = [pred] 
                 
+        return loss
+    
+    
+class CrossEntropyLoss:
+    def __init__(self):
+        self.softmax = SoftMax()
+    
+    def __call__(self, pred, target):
+        backend = pred.backend
+        
+        with Tensor.no_grad():
+            softmax_out = self.softmax(pred).data
+        
+        loss = Tensor(-backend.sum(target.data * backend.log(softmax_out + 1e-9)) / pred.shape[0],
+                      backend=backend,
+                      grad_en=pred.grad_en)
+        
+        if loss.grad_en:
+            loss.grad_fn = lambda grad: [grad * (softmax_out - target.data) / pred.shape[0]]
+            loss._prev = [pred]
+            
         return loss
