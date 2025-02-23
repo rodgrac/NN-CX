dbc9e41610621ef6acd0e79f76a62a3c4ab73724
rodgrac <rodneydsouza92@gmail.com>
2025-02-23 12:20:20 -0800

    Add confusion matrix in viz

--- nncx/visualizer.py
+++ nncx/visualizer.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 from typing import List
 
 
@@ -8,6 +9,8 @@ def visualize_predictions(preds, targets, title='', xlabel='Targets', ylabel='Pr
         preds = np.array(preds)
         targets = np.array(targets)
     
+    
+    print('[Viz] Plotting predictions vs targets...')
     plt.figure(figsize=(10, 6))
     plt.scatter(targets, preds, color='blue', alpha=0.6)
     
@@ -18,4 +21,26 @@ def visualize_predictions(preds, targets, title='', xlabel='Targets', ylabel='Pr
     plt.ylabel(ylabel)
     plt.grid(True)
     plt.show()
+    
+    
+def plot_confusion_matrix(preds, targets, num_classes, class_names):
+    cm = np.zeros((num_classes, num_classes), dtype=int)
+    
+    preds, targets = np.array(preds), np.array(targets)
+    if preds.ndim != 1:     # Sample
+        preds = np.argmax(preds, axis=-1)
+        
+    if targets.ndim != 1:     # Sample
+        targets = np.argmax(targets, axis=-1)
+    
+    for t, p in zip(targets, preds):
+        cm[t, p] += 1
+        
+    plt.figure(figsize=(10, 8))
+    print('[Viz] Plotting confusion matrix...')
+    sns.heatmap(cm, annot=True, fmt='d', cmap="Blues", xticklabels=class_names, yticklabels=class_names)
+    plt.xlabel('Predicted class')
+    plt.ylabel('Actual class')
+    plt.title('Confusion Matrix') 
+    plt.show()   
     
