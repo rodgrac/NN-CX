a79920874cd87ad6ff37e80545d51afe5f096e9d
rodgrac <rodneydsouza92@gmail.com>
2025-02-23 01:27:08 -0800

    Add softmax act layer

--- nncx/layers/activations.py
+++ nncx/layers/activations.py
@@ -54,3 +54,30 @@ class Tanh:
     
     def __repr__(self) -> str:
         return f"Tanh"
+    
+
+class SoftMax:
+    def __call__(self, x):
+        return self.forward(x)
+    
+    def forward(self, x):
+        exp_x = x.backend.exp(x.data - x.backend.max(x.data, axis=-1, keepdims=True))
+        out = Tensor((exp_x / x.backend.sum(exp_x, axis=-1, keepdims=True)),
+                     backend=x.backend, grad_en=x.grad_en)
+        
+        if out.grad_en:
+            bsz, out_sz = out.shape
+            out_grad = x.backend.zeros((bsz, out_sz, out_sz))
+            for i in range(bsz):
+                out_m = out.data[i].reshape(-1, 1)
+                out_grad[i] = x.backend.diagflat(out_m) - x.backend.dot(out_m, out_m.T)
+            
+            out.grad_fn = lambda grad: [grad * out_grad]
+            out._prev = [x]
+            
+        return out
+    
+    def __repr__(self) -> str:
+        return f"SoftMax"
+        
+        
