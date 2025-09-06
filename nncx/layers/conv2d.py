2d1d1dff7aa107a3f4063024b85d5a17b6b6c2c4
rodgrac <rodneydsouza92@gmail.com>
2025-09-06 08:54:02 -0700

    Add conv2d backward impl.

--- nncx/layers/conv2d.py
+++ nncx/layers/conv2d.py
@@ -18,7 +18,7 @@ class Conv2d:
         self.w.data *= (2.0 / (in_channels * kernel_size**2))**0.5
         
         if bias:
-            self.bias = Tensor(shape=(1, out_channels), backend=backend, grad_en=True)
+            self.bias = Tensor(shape=(out_channels,), backend=backend, grad_en=True)
         else:
             self.bias = None
             
@@ -38,10 +38,9 @@ class Conv2d:
         H_out = (H - self.kernel_size + 2 * pad_w) // self.stride + 1
         W_out = (W - self.kernel_size + 2 * pad_w) // self.stride + 1
         
-        out = Tensor(shape=(B, self.out_channels, H_out, W_out), backend=self.backend, grad_en=True)
+        out = Tensor(shape=(B, self.out_channels, H_out, W_out), backend=self.backend, grad_en=x.grad_en)
         
-        if self.pad:
-            x.data = self.backend.pad(x.data, ((0, 0), (0, 0), (pad_w, pad_w), (pad_w, pad_w)))
+        x_pad = self.backend.pad(x.data, ((0, 0), (0, 0), (pad_w, pad_w), (pad_w, pad_w)))
         
         for b in range(B):
             for c in range(self.out_channels):
@@ -50,11 +49,44 @@ class Conv2d:
                         row_s = row * self.stride
                         col_s = col * self.stride
 
-                        out.data[b, c, row, col] = self.backend.sum(x.data[b, :, row_s : row_s + self.kernel_size, 
+                        out.data[b, c, row, col] = self.backend.sum(x_pad[b, :, row_s : row_s + self.kernel_size, 
                                                                            col_s : col_s + self.kernel_size] \
-                                                    * self.w.data[c]) + (self.bias.data[0, c] if self.bias is not None else 0)
+                                                    * self.w.data[c]) + (self.bias.data[c] if self.bias is not None else 0)
                         
+        if out.grad_en:
+            def _backward(grad):                
+                # bias grad
+                if self.bias is not None:
+                    self.bias.grad.fill(0)
+                    
+                    for c in range(self.out_channels):
+                        self.bias.grad[c] = self.backend.sum(grad[:, c, :, :])
+                    
+                # weight and input grad
+                self.w.grad.fill(0)
+                dx = self.backend.zeros(x_pad.shape, x.dtype)
+                for b in range(B):
+                    for c in range(self.out_channels):
+                        for row in range(H_out):
+                            for col in range(W_out):
+                                row_s = row * self.stride
+                                col_s = col * self.stride
+                                region = x_pad[b, :, row_s : row_s + self.kernel_size, 
+                                                col_s : col_s + self.kernel_size]
+                                self.w.grad[c] += region * grad[b, c, row, col]
+                                
+                                dx[b, :, row_s : row_s + self.kernel_size, 
+                                          col_s : col_s + self.kernel_size] += self.w.data[c] * grad[b, c, row, col]
+                                
+                if self.pad:
+                    dx = dx[:, :, pad_w:-pad_w, pad_w:-pad_w]
+                    
+                return [dx]
+            
+            out.grad_fn = _backward
+            out._prev = [x]
+        
         return out
     
-        
-    
+    def __repr__(self):
+        return f"Conv2d"
