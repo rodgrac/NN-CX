f5cd4411e3404add9a726fb3cc99d6c2daee7b25
rodgrac <rodneydsouza92@gmail.com>
2025-09-15 22:20:31 -0700

    Add randomcrop and flip spt in transforms

--- nncx/datasets/transform.py
+++ nncx/datasets/transform.py
@@ -14,8 +14,8 @@ class Standardize(Transform):
         mean = self.mean
         std = self.std
         if tensor.ndim == 3:
-            mean = np.expand_dims(self.mean, axis=(-1, -2))
-            std = np.expand_dims(self.std, axis=(-1, -2))
+            mean = self.mean[:, None, None]
+            std = self.std[:, None, None]
         
         return (tensor - mean) / std
     
@@ -23,8 +23,8 @@ class Standardize(Transform):
         mean = self.mean
         std = self.std
         if tensor.ndim == 3:
-            mean = np.expand_dims(self.mean, axis=(-1, -2))
-            std = np.expand_dims(self.std, axis=(-1, -2))
+            mean = self.mean[:, None, None]
+            std = self.std[:, None, None]
         
         return tensor * std + mean
 
@@ -37,8 +37,12 @@ class Normalize(Transform):
     def __call__(self, tensor):
         return (tensor.astype(np.float32) - self.min_val) / (self.max_val - self.min_val)
     
-    def invert(self, tensor):
-        return tensor.astype(np.float32) * (self.max_val - self.min_val) + self.min_val
+    def invert(self, tensor, dtype=np.uint8):
+        tensor = tensor.astype(np.float32) * (self.max_val - self.min_val) + self.min_val
+        if not np.issubdtype(dtype, np.floating):
+            info_int = np.iinfo(dtype)
+            tensor = np.clip(tensor, info_int.min, info_int.max).astype(dtype)
+        return tensor
     
 
 class Flatten(Transform):
@@ -52,3 +56,32 @@ class OneHotEncode(Transform):
         
     def __call__(self, tensor):
         return self.enc[np.array(tensor)]
+    
+    
+class RandomHorizontalFlip(Transform):
+    def __init__(self, p=0.5):
+        super().__init__()
+        self.p = p
+        
+    def __call__(self, tensor):
+        if np.random.rand() < self.p:
+            tensor = tensor[:, :, ::-1]
+
+        return tensor
+    
+    
+class RandomCrop(Transform):
+    def __init__(self, size, padding=0):
+        super().__init__()
+        self.size = size
+        self.padding = padding
+        
+    def __call__(self, tensor):
+        if self.padding > 0:
+            tensor = np.pad(tensor, ((0, 0), (self.padding, self.padding), (self.padding, self.padding)), mode='constant')
+         
+        _, h, w = tensor.shape   
+        i = np.random.randint(0, h - self.size + 1)
+        j = np.random.randint(0, w - self.size + 1)
+        
+        return tensor[:, i : i + self.size, j : j + self.size]
