c1f723493260cd98a160073c20748ed8ce011163
rodgrac <rodneydsouza92@gmail.com>
2025-09-15 22:19:48 -0700

    Add momentum support in sgd opt

--- nncx/optimizers.py
+++ nncx/optimizers.py
@@ -1,13 +1,25 @@
 class SGD:
-    def __init__(self, params, lr=0.01) -> None:
+    def __init__(self, backend, params, lr=0.01, momentum=0.9) -> None:
+        self.backend = backend
         self.params = params
         self.lr = lr
+        self.momentum = momentum
+        self.v = {}
     
     def step(self):
         for param in self.params:
-            if param.grad is not None:
-                param.data -= self.lr * param.grad
-                param._zero_grad()          
+            if param.grad is None:
+                continue
+            
+            if param not in self.v:
+                self.v[param] = self.backend.zeros(param.shape, param.dtype)
+                
+            v = self.v[param]
+            
+            v[:] = self.momentum * v - self.lr * param.grad
+            param.data += v
+            
+            param._zero_grad()          
                       
     def zero_grad(self):
         for param in self.params:
