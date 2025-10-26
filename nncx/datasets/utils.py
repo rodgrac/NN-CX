5fe810dfa9ad00c0bcfb09c73e88a41d7b66dcab
rodgrac <rodneydsouza92@gmail.com>
2025-10-25 17:12:26 -0700

    Add zipfile support in dataset utils

--- nncx/datasets/utils.py
+++ nncx/datasets/utils.py
@@ -1,40 +1,48 @@
 import os
 import requests
 import tarfile
-import gzip
-import shutil
+import zipfile
 from tqdm import tqdm
 
 
-def download_tar_extract(url, download_path, extract=True):
-    tar_file_name = url.split('/')[-1]
-    extract_folder_name = tar_file_name.split('.')[0]
+def download_extract_file(url, download_path, file_name=None, extract=True):
+    if file_name is None:
+        file_name = url.split('/')[-1]
     
-    if os.path.exists(os.path.join(download_path, extract_folder_name)):
-        print(f'Found {extract_folder_name} already installed')
+    folder_name = file_name.split('.')[0]
+    
+    if os.path.exists(os.path.join(download_path, folder_name)):
+        print(f'Found {folder_name} already installed')
         return
     
-    if not os.path.exists(os.path.join(download_path, tar_file_name)):
+    if not os.path.exists(os.path.join(download_path, file_name)):
         response = requests.get(url, stream=True)
         total_size = int(response.headers.get('content-length', 0))
     
-        with open(os.path.join(download_path, tar_file_name), 'wb') as f, tqdm(
-            desc=f'Downloading {tar_file_name}', total=total_size, unit='B', unit_scale=True
+        with open(os.path.join(download_path, file_name), 'wb') as f, tqdm(
+            desc=f'Downloading {file_name}', total=total_size, unit='B', unit_scale=True
         ) as pbar:
             for data in response.iter_content(chunk_size=1024):
                 f.write(data)
                 pbar.update(len(data))
     
     if extract:
-        if '.'.join(tar_file_name.split('.')[-2:]) == 'tar.gz' or \
-            tar_file_name.split('.')[-1] in ['tar', 'tgz']:
-            with tarfile.open(os.path.join(download_path, tar_file_name), 'r') as tar:
+        if '.'.join(file_name.split('.')[-2:]) == 'tar.gz' or \
+            file_name.split('.')[-1] in ['tar', 'tgz']:
+            with tarfile.open(os.path.join(download_path, file_name), 'r') as tar:
                 total_files = len(tar.getnames())
-                with tqdm(total=total_files, desc=f'Extracting {tar_file_name}', unit='file') as pbar:
+                with tqdm(total=total_files, desc=f'Extracting {file_name}', unit='file') as pbar:
                     for member in tar.getmembers():
                         tar.extract(member, path=download_path)
+                        pbar.update(1)
+        elif file_name.split('.')[-1] == 'zip':
+            with zipfile.ZipFile(os.path.join(download_path, file_name), 'r') as zip:
+                total_files = len(zip.namelist())
+                with tqdm(total=total_files, desc=f'Extracting {file_name}', unit='file') as pbar:
+                    for member in zip.namelist():
+                        zip.extract(member, path=download_path)
                         pbar.update(1)
         else:
             raise NotImplementedError
     
-        os.remove(os.path.join(download_path, tar_file_name))
+        os.remove(os.path.join(download_path, file_name))
