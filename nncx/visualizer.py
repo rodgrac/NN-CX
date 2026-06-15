7570b1032de68b87b94c8a2b4b537902e0bb5a93
rodgrac <rodneydsouza92@gmail.com>
2026-06-14 18:20:50 -0700

    Add detection preds to viz

--- nncx/visualizer.py
+++ nncx/visualizer.py
@@ -6,6 +6,7 @@ import seaborn as sns
 from typing import List
 
 from nncx.datasets.dataset import Dataset
+from nncx.utils import sigmoid
 
 def view_image_dataset(dataset: Dataset, grid_size=(5, 5)):
     fig, axs = plt.subplots(*grid_size, figsize=(8, 8))
@@ -84,21 +85,35 @@ def visualize_predictions(model, dataloader, num_samples=9):
     
     grid_size = int(np.ceil(np.sqrt(num_samples)))
     
-    inputs, targets = next(iter(dataloader))
-    idxs = np.random.choice(inputs.shape[0], num_samples, replace=False)
+    # sample a random batch
+    num_workers_tmp = dataloader.num_workers
+    dataloader.num_workers = 0
+    batch_idx = random.randint(0, len(dataloader) - 1)
+    for i, batch in enumerate(dataloader):
+        if i == batch_idx:
+            inputs, targets = batch
+            break
+        
+    sample_idxs = np.random.choice(inputs.shape[0], num_samples, replace=False)
     
-    inputs_s, targets_s = inputs[idxs], targets[idxs]
+    inputs_s = inputs[sample_idxs]
+    targets_s =  tuple(t[sample_idxs] for t in targets)
     
     preds_s = model.predict(inputs_s)
     
-    inputs_s, preds_s, targets_s = inputs_s.get(), preds_s.get(), targets_s.get()
+    inputs_s, preds_s, targets_s = inputs_s.get(), tuple(p.get() for p in preds_s), tuple(t.get() for t in targets_s)
     
-    if targets_s.ndim != 1:
-        targets_s = np.argmax(targets_s, axis=-1)
+    # if targets_s.ndim != 1:
+    #     targets_s = np.argmax(targets_s, axis=-1)
+    
+    pred_boxes, pred_confs = preds_s
+    gt_boxes, _ = targets_s
     
     plt.figure(figsize=(grid_size * 2, grid_size * 2))
-    for i, idx in enumerate(idxs):
-        img = inputs_s[i].reshape(dataloader.dataset.image_size)
+    for i in range(num_samples):
+        img = inputs_s[i]
+        if img.ndim != 3:
+           img = img.reshape(dataloader.dataset.image_size)
         
         # Invert any transforms
         for transform in dataloader.dataset.transforms_inputs[::-1]:    # Reverse transform order
@@ -108,8 +123,37 @@ def visualize_predictions(model, dataloader, num_samples=9):
         plt.subplot(grid_size, grid_size, i + 1)
         plt.imshow(img.transpose(1, 2, 0))   # HWC
         plt.axis('off')
-        plt.title(f"Pred: {dataloader.dataset.label_names[int(preds_s[i])]}\nTrue: {dataloader.dataset.label_names[int(targets_s[i])]}", 
-                  fontsize=10, color='green' if preds_s[i] == targets_s[i] else 'red')
+        
+        
+        H, W = img.shape[-2], img.shape[-1]
+        # Predicted boxes
+        conf = sigmoid(pred_confs[i])
+        if conf < 0.5:
+            continue
+        cx, cy, bw, bh = pred_boxes[i].flatten()
+        x1, y1 = (cx - bw / 2) * W, (cy - bh / 2) * H
+        plt.gca().add_patch(plt.Rectangle(
+            (x1, y1), bw * W, bh * H,
+            fill=False, color='lime', lw=2
+        ))
+        plt.text(x1, y1, f"{float(conf):.2f}", color='yellow', fontsize=8)
+
+        # Ground truth boxes
+        cx, cy, bw, bh = gt_boxes[i].flatten()
+        x1, y1 = (cx - bw / 2) * W, (cy - bh / 2) * H
+        plt.gca().add_patch(plt.Rectangle(
+            (x1, y1), bw * W, bh * H,
+            fill=False, color='red', lw=1.5, linestyle='--'
+        ))
+
+        plt.title(f"Conf ≥ {0.5}", fontsize=9)
+        
+        #FIXME: cls disabled. COver both cls and detect in this method
+        # plt.title(f"Pred: {dataloader.dataset.label_names[int(preds_s[i])]}\nTrue: {dataloader.dataset.label_names[int(targets_s[i])]}", 
+        #           fontsize=10, color='green' if preds_s[i] == targets_s[i] else 'red')
     
     plt.tight_layout()
     plt.show()
+    
+    # Revert num workers
+    dataloader.num_workers = num_workers_tmp
